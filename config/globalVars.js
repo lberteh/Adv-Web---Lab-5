8d7a7398c8811670121c388e5b09a200c7603c2c
Lucas Schoenardie <lberteh@gmail.com>
2016-11-04 23:01:46 -0400

    First commit, starting from lesson 8 files

---
+++ config/globalVars.js
@@ -0,0 +1,6 @@
+// global variables for the application
+module.exports = {
+    // db: 'mongodb://localhost/comp2068'
+    db: 'mongodb://lberteh:lb885642lb@ds015194.mlab.com:15194/comp2068',
+    secret: 'dbkjd  s7d8 89 (*G(U  09n ds09()))' // used to create salt
+};
